9d141316f5b765e1e77ea0ac8a3c1e4c1abf397f
Argus <143566082+ArgussNik@users.noreply.github.com>
2026-08-25 10:35:43 -0300

    Fix formatting and add client initialization
    Signed-off-by: Argus <143566082+ArgussNik@users.noreply.github.com>

--- js/supabase.js
+++ js/supabase.js
@@ -1,2 +1,4 @@
 const SUPABASE_URL = https://unrzglexlphrytpagswy.supabase.co/rest/v1/
 const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVucnpnbGV4bHBocnl0cGFnc3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwMzE5MDUsImV4cCI6MjA5ODYwNzkwNX0.pE4M3Mcu-QOAK8wwtMDeEFuvkq6OPEFflDlyPytIQpA
+
+const s_client = supabase.createClient(supabaseUrl, supabaseKey);
